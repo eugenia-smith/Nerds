639cf22f59b72122f8cb133952e3087fe018ec51
eugenia-smith <ev-genius4@mail.ru>
2022-09-08 15:16:08 +0300

    Slider script added

--- js/slider.js
+++ js/slider.js
@@ -1,14 +1,33 @@
 'use strict';
 
-let sliders = Array.from(document.querySelectorAll('.slider'));
-let controls = Array.from(document.querySelector('.slider-controls').getElementsByTagName('button'));
+let slideIndex = 0;
+showSlides();
 
-let n = 0;
-
-for (let i = 0; i < controls.length; i++) {
+/*for (let i = 0; i < controls.length; i++) {
   controls[i].addEventListener('click', function (evt) {
     evt.preventDefault();
     //console.log(this);
     this.classList.add('current');
   });
+}*/
+
+function showSlides() {
+  let slides = Array.from(document.querySelectorAll('.slide'));
+  let controls = Array.from(document.querySelector('.slider-controls').getElementsByTagName('button'));
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+
+
+  if (slideIndex > slides.length) {
+    slideIndex = 1};
+
+  for (let i = 0; i < controls.length; i++) {
+    controls[i].classList.remove('current');
+  };
+
+  slides[slideIndex - 1].style.display = "flex";
+  controls[slideIndex - 1].classList.add('current');
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
